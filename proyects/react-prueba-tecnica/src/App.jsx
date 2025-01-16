b9f3fd1c1a51c3fd1fdb117a7e48aafbaa3735b4
campo22 <miguelcampodiaz@gmail.com>
2025-01-15 21:09:21 -0500

    agregando Custom Hooks

--- proyects/react-prueba-tecnica/src/App.jsx
+++ proyects/react-prueba-tecnica/src/App.jsx
@@ -1,43 +1,20 @@
-import { useEffect, useState } from "react";
 import './App.css';
-
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
-const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com';
+import { useCatImage } from "./hooks/usecatImage";
+import { useCatFact } from "./hooks/useCatFact";
 
 export default function App() {
-    const [fact, setFact] = useState('');
-    const [image, setImage] = useState('');
+    const { fact, getRandomFactAndUpdateState } = useCatFact()
+    const { image } = useCatImage({ fact })
 
-    useEffect(() => {
-        fetch(CAT_ENDPOINT_RANDOM_FACT)
-            .then(response => response.json())
-            .then(data => {
-                const { fact } = data;
-                setFact(fact);
-            })
-            .catch(error => console.error('Error fetching cat fact:', error));
-    }, []);
 
-    useEffect(() => {
-        if (!fact) return;
-
-        const firstWord = fact.split(" ").slice(0, 3).join(" ");
-        console.log(firstWord);
-
-        fetch(`${CAT_ENDPOINT_IMAGE_URL}/cat/says/${firstWord}?size=50&color=blue`)
-            .then(response => {
-                if (response.ok) {
-                    setImage(response.url); // Usa directamente response.url
-                } else {
-                    console.error('Error fetching image');
-                }
-            })
-            .catch(error => console.error('Error fetching image:', error));
-    }, [fact]); // Corrección de dependencia
+    const handleClick = async () => {
+        getRandomFactAndUpdateState()
+    }
 
     return (
         <main>
             <h1>App de gatos</h1>
+            <button onClick={handleClick}>Get new fact</button>
             {fact && <p>{fact}</p>}
             {image && <img src={image} alt={`Imagen de un gato que dice: ${fact}`} />}
         </main>
